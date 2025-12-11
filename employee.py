8795ab6ac7c99fc162425c1ac5dec27fbd2018d9
patilbhimangoud42-cyber <patilbhimangoud42@gmail.com>
2025-12-11 15:44:21 +0530

    Update employee.py

--- employee.py
+++ employee.py
@@ -1,16 +1,15 @@
-def employee_details(name, emp_id, department, salary):
+def employee_details(name,emp_id,department,salary):
     result = (
-        f"Employee Name: {name}\n"
-        f"Employee ID: {emp_id}\n"
-        f"Department: {department}\n"
-        f"Salary: {salary}\n"
+        f"Employee_Name:{name}\n"
+        f"Employee_ID:{emp_id}\n"
+        f"Department:{department}\n"
+        f"Salary:{salary}"
     )
     return result
 
-
-if __name__ == "__main__":
-    name = "Alice"
-    emp_id = "E1001"
-    department = "IT"
-    salary = 50000
-    print(employee_details(name, emp_id, department, salary))
+if ___name___ == "___main___":
+    name="Alice"
+    emp_id="E1001"
+    department="IT"
+    salary=55000
+    print(employee_details(name,emp_id,department,salary))
